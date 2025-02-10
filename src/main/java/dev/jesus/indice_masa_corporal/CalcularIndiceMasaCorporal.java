85a4a3d1553fcad7ee3f7fb2cb9472f92ce42a72
JesusEnjamio <jenjamiob@gmail.com>
2025-02-10 22:26:23 +0100

    testClasificarIMC is working

--- src/main/java/dev/jesus/indice_masa_corporal/CalcularIndiceMasaCorporal.java
+++ src/main/java/dev/jesus/indice_masa_corporal/CalcularIndiceMasaCorporal.java
@@ -8,5 +8,25 @@ public class CalcularIndiceMasaCorporal {
         return peso / Math.pow(estatura, 2);
     }
 
+    public static String clasificarIMC(double imc) {
+        if (imc < 16) {
+            return "Delgadez severa";
+        } else if (imc < 17) {
+            return "Delgadez moderada";
+        } else if (imc < 18.5) {
+            return "Delgadez leve";
+        } else if (imc < 25) {
+            return "Peso normal";
+        } else if (imc < 30) {
+            return "Sobrepeso";
+        } else if (imc < 35) {
+            return "Obesidad leve";
+        } else if (imc < 40) {
+            return "Obesidad moderada";
+        } else {
+            return "Obesidad mórbida";
+        }
+    }
+
 
 }
