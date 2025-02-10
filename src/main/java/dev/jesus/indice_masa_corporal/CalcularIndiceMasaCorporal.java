04753eba27cfe2e8f046ace65d3fbfbf07985e72
JesusEnjamio <jenjamiob@gmail.com>
2025-02-10 22:22:57 +0100

    testCalcularImc is working

--- src/main/java/dev/jesus/indice_masa_corporal/CalcularIndiceMasaCorporal.java
+++ src/main/java/dev/jesus/indice_masa_corporal/CalcularIndiceMasaCorporal.java
@@ -1,6 +1,12 @@
 package dev.jesus.indice_masa_corporal;
 
 public class CalcularIndiceMasaCorporal {
-    
+    public static double calcularIMC(double peso, double estatura) {
+        if (peso <= 0 || estatura <= 0) {
+            throw new IllegalArgumentException("Peso y estatura deben ser mayores a cero.");
+        }
+        return peso / Math.pow(estatura, 2);
+    }
+
 
 }
