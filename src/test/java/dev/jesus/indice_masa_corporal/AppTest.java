22a1803ae6100f64e9ef7c762f0c890382f599b4
JesusEnjamio <jenjamiob@gmail.com>
2025-02-10 22:28:30 +0100

    testMain doesnt work

--- src/test/java/dev/jesus/indice_masa_corporal/AppTest.java
+++ src/test/java/dev/jesus/indice_masa_corporal/AppTest.java
@@ -1,7 +1,15 @@
 package dev.jesus.indice_masa_corporal;
-
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class AppTest {
 
-   
+    @Test
+    public void testMainEjecutaSinErrores() {
+        String input = "70\n1.75\n";  
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        assertDoesNotThrow(() -> App.main(new String[]{}));
+    }
 }
